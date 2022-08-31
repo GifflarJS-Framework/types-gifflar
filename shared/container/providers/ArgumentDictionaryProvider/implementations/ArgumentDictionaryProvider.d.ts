31207ea8e09f9693c010005c1b604d69cd4ba226
Levy Santiago <levyssantiago@gmail.com>
2022-08-31 09:03:53 -0300

    feat: adding script types

--- shared/container/providers/ArgumentDictionaryProvider/implementations/ArgumentDictionaryProvider.d.ts
+++ shared/container/providers/ArgumentDictionaryProvider/implementations/ArgumentDictionaryProvider.d.ts
@@ -8,17 +8,19 @@ import { IWriteContractsCommand } from "@modules/commands/WriteContracts/types/I
 import { ICreateContractModelCommand } from "@modules/commands/CreateContractModel/types/ICreateContractModelCommand";
 import { ICompileContractsCommand } from "@modules/commands/CompileContracts/types/ICompileContractsCommand";
 import { IDeployContractsCommand } from "@modules/commands/DeployContracts/types/IDeployContractsCommand";
+import { ICreateScriptCommand } from "@modules/commands/CreateScript/types/ICreateScriptCommand";
 export default class ArgumentDictionaryProvider implements IArgumentDictionaryProvider {
     private helpCommand;
     private initCommand;
     private createContractModelCommand;
     private createServiceCommand;
+    private createScriptCommand;
     private writeContractsCommand;
     private compileContractsCommand;
     private deployContractsCommand;
     private argumentsDictionary;
     private aliasesDictionary;
-    constructor(helpCommand: IHelpCommand, initCommand: IInitCommand, createContractModelCommand: ICreateContractModelCommand, createServiceCommand: ICreateServiceCommand, writeContractsCommand: IWriteContractsCommand, compileContractsCommand: ICompileContractsCommand, deployContractsCommand: IDeployContractsCommand);
+    constructor(helpCommand: IHelpCommand, initCommand: IInitCommand, createContractModelCommand: ICreateContractModelCommand, createServiceCommand: ICreateServiceCommand, createScriptCommand: ICreateScriptCommand, writeContractsCommand: IWriteContractsCommand, compileContractsCommand: ICompileContractsCommand, deployContractsCommand: IDeployContractsCommand);
     private registryAlias;
     getArgInfoByReceivedArg({ receivedArgKey, }: IGetArgInfoByReceivedArgDTO): IDictionaryItemInfo | undefined;
 }
--- utils/files/index.d.ts
+++ utils/files/index.d.ts
@@ -16,6 +16,6 @@ export declare const makeDirectory: ({ path }: IMakeDirectoryDTO) => void;
 export declare const copyFile: ({ sourcePath, destPath }: ICopyFileDTO) => void;
 export declare const writeFile: ({ destPath, content }: IWriteFileDTO) => void;
 export declare const fileExists: ({ path }: IPathInput) => boolean;
-export declare const readFile: ({ path }: IPathInput) => any | null;
+export declare const readFile: ({ path }: IPathInput) => string | undefined;
 export declare const listFolderFiles: ({ path }: IPathInput) => any | null;
 export {};
