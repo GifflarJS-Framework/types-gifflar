6dece164f7fa446a7d2663a035b15fb8d904cdc6
Levy Santiago <levyssantiago@gmail.com>
2022-09-01 10:33:39 -0300

    refactor: updating types

--- modules/commands/CreateScript/implementations/CreateScriptCommand.d.ts
+++
@@ -1,5 +0,0 @@
-import { ICreateScriptCommand } from "../types/ICreateScriptCommand";
-declare class CreateScriptCommand implements ICreateScriptCommand {
-    execute(value: string): Promise<void>;
-}
-export default CreateScriptCommand;
--- modules/commands/index.d.ts
+++
@@ -1,8 +0,0 @@
-import "./CreateContractModel";
-import "./CreateService";
-import "./WriteContracts";
-import "./CompileContracts";
-import "./DeployContracts";
-import "./CreateScript";
-import "./Help";
-import "./Init";
